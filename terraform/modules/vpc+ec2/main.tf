70804c653925ddb44fdde750eb6a21f1d13cf546
Anil bhau <anil@gmail.com>
2025-05-16 11:43:47 +0530

    tf_code_added| AB

--- terraform/modules/vpc+ec2/main.tf
+++ terraform/modules/vpc+ec2/main.tf
@@ -1,48 +1,48 @@
-provider "aws" {
-    region = "ap-southeast-2"
+# provider "aws" {
+#     region = "ap-southeast-2"
 
-}
-
-resource "aws_vpc" "my_vpc" {
-    cidr_block = "10.0.0.0/16"
-  
-}
-
-# resource "aws_vpc" "main" {
-#   cidr_block = "10.0.0.0/16"
 # }
 
-resource "aws_subnet" "my_vpc-sub" {
-  cidr_block = "10.0.0.0/20"
-  vpc_id = aws_vpc.my_vpc.id
+# resource "aws_vpc" "my_vpc" {
+#     cidr_block = "10.0.0.0/16"
+  
+# }
 
-  tags= {
-    Name = "public subnet"  
+# # resource "aws_vpc" "main" {
+# #   cidr_block = "10.0.0.0/16"
+# # }
 
-  }
-}
+# resource "aws_subnet" "my_vpc-sub" {
+#   cidr_block = "10.0.0.0/20"
+#   vpc_id = aws_vpc.my_vpc.id
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+#   tags= {
+#     Name = "public subnet"  
 
-  tags = {
-    Name = "my_igw"
-  }
-}
+#   }
+# }
 
-resource "aws_default_route_table" "example" {
-  default_route_table_id = aws_vpc.example.default_route_table_id
+# resource "aws_internet_gateway" "my_igw" {
+#   vpc_id = aws_vpc.my_vpc.id
 
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
-  }
+#   tags = {
+#     Name = "my_igw"
+#   }
+# }
+
+# resource "aws_default_route_table" "example" {
+#   default_route_table_id = aws_vpc.example.default_route_table_id
+
+#   route {
+#     cidr_block = "10.0.1.0/24"
+#     gateway_id = aws_internet_gateway.example.id
+#   }
 
 
-  tags = {
-    Name = "example"
-  }
-}
+#   tags = {
+#     Name = "example"
+#   }
+# }
 
 
 
